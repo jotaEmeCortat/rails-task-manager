024d38750df7faab76bfa30d835fcda59fb4a065
jotaEmeCortat <jmcortat@gmail.com>
2025-02-23 20:04:49 -0300

    update Gemfile, some front (view), route logic, controller logic and create a README

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
-
-  get 'tasks/new', to: 'tasks#new'
-  post 'tasks', to: 'tasks#create'
-
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
-  patch 'tasks/:id', to: 'tasks#update'
-
-  delete 'tasks/:id', to: 'tasks#destroy', as: :delete
-
-  get 'tasks/:id', to: 'tasks#show', as: :task
+  resources :tasks
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
